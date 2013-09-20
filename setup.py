73adf6154ea5402cb7b8cea4403e905f678c6173
Moriyoshi Koizumi <mozo@mozo.jp>
2013-09-20 10:20:57 +0900

    Bump version.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ testing_extras = tests_require + [
 
 setup(
     name='pyramid_mailer',
-    version='0.13moriyoshi4',
+    version='0.13moriyoshi5',
     license='BSD',
     author='Dan Jacob',
     author_email='danjac354@gmail.com',
